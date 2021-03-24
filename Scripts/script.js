ee716c6a3ad3bdc7f156854dea3efa17bb4eced5
Shpend Mula <shmula@hotmail.com>
2021-03-24 20:45:26 +0100

    eightpoint control completed

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,11 +12,23 @@ const fullControlBtnSwitcher = document.querySelector(".full-control-btn-switche
 const customDimensions = document.querySelector('.dimensions-inputs-container');
 const shapeWidth = document.getElementById("width");
 const shapeHeight = document.getElementById("height");
+const copyToClipboardBtn = document.querySelector('.copy-to-clipboard-btn');
 
-var topLeft = 0;
+let topLeft = 0;
 let topRight = 0;
 let bottomRight = 0;
 let bottomLeft = 0;
+
+let topLeftH = 0;
+let topLeftV = 0;
+let topRightH = 0;
+let topRightV = 0;
+let bottomRightH = 0;
+let bottomRightV = 0;
+let bottomLeftH = 0;
+let bottomLeftV = 0;
+
+
 
 allSliders.forEach(slider => {
     slider.addEventListener('input', fourPointControl)
@@ -47,13 +59,54 @@ function fourPointControl() {
 }
 
 function eightPointControl() {
-    console.log('hello')
+    if(this.dataset.shapeCorner === 'top-left-h') {
+        topLeftH = this.value;
+        eightPointValues.innerHTML = `${this.value}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+        generatedShape.style.borderRadius = `${this.value}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+
+    }
+    else if(this.dataset.shapeCorner === 'top-left-v') {
+        topLeftV = this.value;
+        eightPointValues.innerHTML = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${this.value}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+        generatedShape.style.borderRadius = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${this.value}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+    }
+    else if(this.dataset.shapeCorner === 'top-right-h') {
+        topRightH = this.value;
+        eightPointValues.innerHTML = `${topLeftH}% ${this.value}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+        generatedShape.style.borderRadius = `${topLeftH}% ${this.value}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+    }
+    else if(this.dataset.shapeCorner === 'top-right-v') {
+        topRightV = this.value;
+        eightPointValues.innerHTML = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${this.value}% ${bottomRightV}% ${bottomLeftV}%`;
+        generatedShape.style.borderRadius = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${this.value}% ${bottomRightV}% ${bottomLeftV}%`;
+    }
+    else if(this.dataset.shapeCorner === 'bottom-right-h') {
+        bottomRightH = this.value;
+        eightPointValues.innerHTML = `${topLeftH}% ${topRightH}% ${this.value}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+        generatedShape.style.borderRadius = `${topLeftH}% ${topRightH}% ${this.value}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+    }
+    else if(this.dataset.shapeCorner === 'bottom-right-v') {
+        bottomRightV = this.value;
+        eightPointValues.innerHTML = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${this.value}% ${bottomLeftV}%`;
+        generatedShape.style.borderRadius = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${this.value}% ${bottomLeftV}%`;
+    }
+    else if(this.dataset.shapeCorner === 'bottom-left-h') {
+        bottomLeftH = this.value;
+        eightPointValues.innerHTML = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${this.value}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+        generatedShape.style.borderRadius = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${this.value}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${bottomLeftV}%`;
+    }
+    else if(this.dataset.shapeCorner === 'bottom-left-v') {
+        bottomLeftV = this.value;
+        eightPointValues.innerHTML = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${this.value}%`;
+        generatedShape.style.borderRadius = `${topLeftH}% ${topRightH}% ${bottomRightH}% ${bottomLeftH}% / ${topLeftV}% ${topRightV}% ${bottomRightV}% ${this.value}%`;
+    }
 }
 
 customSizeBtn.addEventListener('click', () => {
     btnSwitcher.classList.toggle('btn-switcher-active');
     customDimensions.classList.toggle('custom-dimensions-active');
 })
+
 
 fullControlBtn.addEventListener('click',() => {
     fullControlBtnSwitcher.classList.toggle('btn-switcher-active');
@@ -73,11 +126,29 @@ fullControlBtn.addEventListener('click',() => {
     })
 
     radiusValues.innerHTML = `${0}% ${0}% ${0}% ${0}%`;
+    eightPointValues.innerHTML = `${0}% ${0}% ${0}% ${0}% / ${0}% ${0}% ${0}% ${0}%`;
+
+    generatedShape.style.borderRadius = `${0}% ${0}% ${0}% ${0}%`;
+
+     topLeft = 0;
+     topRight = 0;
+     bottomRight = 0;
+     bottomLeft = 0;
+
+     topLeftH = 0;
+     topLeftV = 0;
+     topRightH = 0;
+     topRightV = 0;
+     bottomRightH = 0;
+     bottomRightV = 0;
+     bottomLeftH = 0;
+     bottomLeftV = 0;
 
     eightPointSliders.forEach(eightPointSlider => {
         eightPointSlider.addEventListener('input', eightPointControl)
     })
     //If you access to a DOM Element via JS(using for example getElementById) you'll not be able to read the computed style of that element, because it is defined inside the CSS file. To avoid this, you have to use property getComputedStyle(or currentStyle for IE). StackOverflow
+
     if(window.getComputedStyle(eightPointDisplay,null).display === 'none') {
         eightPointDisplay.style.display = "block";
     } else {
@@ -93,3 +164,31 @@ shapeWidth.addEventListener('input', (ev) => {
 shapeHeight.addEventListener('input', (ev) => {
     generatedShape.style.height = ev.target.value + 'px';
 })
+
+
+// copyToClipboardBtn.addEventListener('click', copyToClipBoard);
+
+// function copyToClipBoard() {
+//    if(window.getComputedStyle(eightPointValues,null).display === 'none') {
+    
+//         let borderRadiusVal = document.querySelector('[data-radius-values]'); //select the element
+//         let elementText = borderRadiusVal.textContent; //get the text content from the element
+//         copyText(elementText); //use the copyText function below
+      
+      
+//       //If you only want to put some Text in the Clipboard just use this function
+//       // and pass the string to copied as the argument.
+//       function copyText(text) {
+//         navigator.clipboard.writeText(text);
+//         alert('Copied the text:' + text);
+//       }
+
+//     } else {
+//         let str = document.querySelector('[data-radius-values-eight-points]').innerHTML;
+//         const el = document.createElement('textarea');
+//         el.value = str;
+//         el.select();
+//         document.execCommand('copy');
+//         alert('Copied the text:' + el.value);
+//     }
+// }
